0b360e854c5d0155fb1f0c7a83c4c1fb576d1377
sameeramp <sameeramp2000@gmail.com>
2025-10-18 16:18:54 +0530

    lesson routes refactored

--- controller/lesson/create.js
+++ controller/lesson/create.js
@@ -7,16 +7,18 @@ export const createLesson = async (req, res) => {
         const {
             lesson,
             course,
+            content,
         } = req.body;
 
         if (!course.trim()) return res.status(409).send('Course is not found');
+        if (!content.trim()) return res.status(409).send('No Lesson content is found');
         const courseId = mongoose.Types.ObjectId(course);
         // Check Lesson exists or not
         const lessonData = await Lesson.findOne({ lesson })
         const courseData = await Course.findById(courseId);
         if(!courseData) return res.status(409).send('Course is not found')
         if (lessonData) return res.status(409).send('Lesson is already registered, Please try other')
-        const lessonDataPayload = await Lesson.create({ lesson, course: courseId });
+        const lessonDataPayload = await Lesson.create({ lesson, course: courseId, content });
         const lessonRes = await lessonDataPayload.save();
         return res.status(201).json({...lessonRes._doc, msg: "Created Successfully"});
     } catch (error) {
--- controller/lesson/update.js
+++ controller/lesson/update.js
@@ -1,20 +1,32 @@
 import mongoose from 'mongoose';
 import { Lesson } from '../../models/lesson.js';
+import { Course } from '../../models/course.js';
 
 export const updateLesson = async (req, res) => {
     try {
         const {
             id,
             lesson,
+            content,
+            course,
         } = req.body;
 
-        // convert to mongo id
-        const _id = new mongoose.Types.ObjectId(id);        
-        // Check lesson exists or not
-        const lessonData = await Lesson.findById({_id});        
+        if (!content.trim()) return res.status(409).send('No Lesson content is found');
+        const courseId = mongoose.Types.ObjectId(course);
+        const _id = mongoose.Types.ObjectId(id);
+        // Check Lesson exists or not
+        if (course.trim()) {
+            const courseData = await Course.findById(courseId);
+            if (!courseData) return res.status(409).send('Course is not found')
+        }
+        const lessonData = await Lesson.findById({ _id });
         if (!lessonData) return res.status(409).send('Lesson is not found, please check the id')
-        const lessonRes = await Lesson.findByIdAndUpdate({_id}, {lesson}, {new: true});
-        return res.status(200).json({...lessonRes._doc, msg: "Updated Successfulyy"});
+        const payload = {};
+        if (course.trim()) payload[course] = courseId;
+        if (content.trim()) payload[content] = content;
+        if (lesson.trim()) payload[lesson] = lesson;
+        const lessonRes = await Lesson.findByIdAndUpdate({ _id }, payload, { new: true });
+        return res.status(200).json({ ...lessonRes._doc, msg: "Updated Successfulyy" });
     } catch (error) {
         console.log(error, 'error');
         res.status(500).send("Failed, please try later")
