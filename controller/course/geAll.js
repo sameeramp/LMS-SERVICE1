31692ed562baf12b5c5a53a2966d91555e39dabf
sameeramp <sameeramp2000@gmail.com>
2025-10-20 21:21:21 +0530

    Refactored getAll api

--- controller/course/geAll.js
+++ controller/course/geAll.js
@@ -4,18 +4,13 @@ import { Course } from '../../models/course.js';
 export const getAllCourse = async (req, res) => {
     try {
         const {
-            id,
             page = 1,
             perPage = 10
         } = req.query;
-        if (!id) return res.status(409).send('No id passed, please check the id')
         const limit = perPage;
         const skip = (page - 1) * perPage;
-        // convert to mongo id
-        const _id = new mongoose.Types.ObjectId(id);
-        // Check course exists or not   
         const courseRes = await Course.find().skip(skip).limit(limit);
-        if (!courseRes) return res.status(409).send('No recorde found, please check the id')
+        if (!courseRes) return res.status(409).send('No record found, please check the id')
         return res.status(200).json({ course: courseRes, msg: "Fetched Successfulyy" });
     } catch (error) {
         console.log(error, 'error');
--- controller/lesson/geAll.js
+++ controller/lesson/geAll.js
@@ -4,16 +4,11 @@ import { Lesson } from '../../models/lesson.js';
 export const getAllLesson = async (req, res) => {
     try {
         const {
-            id,
             page = 1,
             perPage = 10
         } = req.query;
-        if (!id) return res.status(409).send('No id passed, please check the id')
         const limit = perPage;
         const skip = (page - 1) * perPage;
-        // convert to mongo id
-        const _id = new mongoose.Types.ObjectId(id);
-        // Check lesson exists or not   
         const lessonRes = await Lesson.find().skip(skip).limit(limit);
         if (!lessonRes) return res.status(409).send('No recorde found, please check the id')
         return res.status(200).json({ lesson: lessonRes, msg: "Fetched Successfulyy" });
